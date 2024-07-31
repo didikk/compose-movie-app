8353c66c41360eeeea6ed3fb124bad53475e94b9
Didik Ismawanto <didik.ismawanto@sicepat.com>
2024-07-31 14:53:40 +0700

    chore: update favorite screen

--- app/src/main/java/com/example/mymovie/presentation/screens/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/example/mymovie/presentation/screens/favorite/FavoriteScreen.kt
@@ -1,17 +1,16 @@
 package com.example.mymovie.presentation.screens.favorite
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.example.mymovie.presentation.components.Header
-import com.example.mymovie.presentation.components.MovieItem
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun FavoriteScreen() {
+fun FavoriteScreen(viewModel: FavoriteViewModel = koinViewModel()) {
+    val favoriteMovies by viewModel.favoriteMovies.collectAsState()
+
     Column {
         Header(title = "Favorite")
 //        LazyColumn(contentPadding = PaddingValues(top = 24.dp)) {
